bfadd688e40876f9e9aaaa97937fca5e6d99c026
Munya <munyaradziMagura@outlook.com>
2024-04-25 19:28:09 +0930

    added a custom list maker

--- pomodoro/src/main.rs
+++ pomodoro/src/main.rs
@@ -1,6 +1,9 @@
 slint::include_modules!();
-
 fn main() -> Result<(), slint::PlatformError> {
+    // this vector will store all of the users tasks
+    
+
+
     let ui: AppWindow = AppWindow::new()?;
 
     // ui.on_request_increase_value({
@@ -10,6 +13,12 @@ fn main() -> Result<(), slint::PlatformError> {
     //         ui.set_counter(ui.get_counter() + 1);
     //     }
     // });
-
+    ui.current_task(move |string|{
+        let ui = ui_handle.unwrap();
+        let parsed = string.trim().unwrap();
+        let task_list = get_collected_tasks();
+        println!(task_list);
+        
+    });
     ui.run()
 }
