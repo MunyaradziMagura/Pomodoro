aa48c6693d2aec887a5f3f80efbe28cc660dd00d
Munya <munyaradziMagura@outlook.com>
2024-04-26 01:08:32 +0930

    made task taker take up 1/ 3 of the screen and the section for the pomodoro take up 2/3 of the screen

--- pomodoro/src/main.rs
+++ pomodoro/src/main.rs
@@ -1,4 +1,4 @@
-use std::rc::Rc;
+use std::{rc::Rc};
 
 use slint::{ModelRc, VecModel};
 
@@ -10,25 +10,25 @@ fn main() -> Result<(), slint::PlatformError> {
     let ui: AppWindow = AppWindow::new().unwrap();
 
     // https://docs.rs/slint/latest/slint/struct.ModelRc.html
-    //let collected_tasks_clone = ui.get_collected_tasks().clone();
     let the_model : Rc<VecModel<slint::SharedString>> =
     Rc::new(VecModel::from(vec!["Hello".into(), "World".into()]));
 // Convert it to a ModelRc.
 let the_model_rc = ModelRc::from(the_model.clone());
 // Pass the model to the ui: The generated set_the_model setter from the
 // the_model property takes a ModelRc.
+let cloned_model_for_remove = the_model.clone(); // Clone for use inside the remove-task closure
 
 ui.set_collected_tasks(the_model_rc);
 
-
     ui.on_current_task(move |string|{
-
       // Trim the parsed string
     let parsed = string.trim().to_string();
     println!("{}", parsed);
     the_model.push(parsed.into());
-
     
+    });
+    ui.on_remove_task(move |idx| {
+        cloned_model_for_remove.remove(idx.try_into().unwrap());
     });
     ui.run()
 }
