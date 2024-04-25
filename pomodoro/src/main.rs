86ddcf09e58601ce4557dfa72103d4c511c3e3be
Munya <munyaradziMagura@outlook.com>
2024-04-25 23:33:05 +0930

    we are now able to add items to the list of tasks and have said items disply on the screen

--- pomodoro/src/main.rs
+++ pomodoro/src/main.rs
@@ -1,22 +1,34 @@
+use std::rc::Rc;
+
+use slint::{ModelRc, SharedVector, VecModel};
+
 slint::include_modules!();
+
 fn main() -> Result<(), slint::PlatformError> {
     // this vector will store all of the users tasks
     
+    let ui: AppWindow = AppWindow::new().unwrap();
+
+    // https://docs.rs/slint/latest/slint/struct.ModelRc.html
+    //let collected_tasks_clone = ui.get_collected_tasks().clone();
+    let the_model : Rc<VecModel<slint::SharedString>> =
+    Rc::new(VecModel::from(vec!["Hello".into(), "World".into()]));
+// Convert it to a ModelRc.
+let the_model_rc = ModelRc::from(the_model.clone());
+// Pass the model to the ui: The generated set_the_model setter from the
+// the_model property takes a ModelRc.
+
+ui.set_collected_tasks(the_model_rc);
 
 
-    let ui: AppWindow = AppWindow::new()?;
-
-    // ui.on_request_increase_value({
-    //     let ui_handle = ui.as_weak();
-    //     move || {
-    //         let ui = ui_handle.unwrap();
-    //         ui.set_counter(ui.get_counter() + 1);
-    //     }
-    // });
     ui.on_current_task(move |string|{
-        let parsed = string.trim();
-        println!("{}", parsed);
-        
+
+      // Trim the parsed string
+    let parsed = string.trim().to_string();
+    println!("{}", parsed);
+    the_model.push(parsed.into());
+
+    
     });
     ui.run()
 }
