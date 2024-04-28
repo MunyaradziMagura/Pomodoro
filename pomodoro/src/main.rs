b9bdb0a5d32accbabc4002a4a1b4d34c4382d0b8
Munya <munyaradziMagura@outlook.com>
2024-04-28 15:20:20 +0930

    chnaged the name if the tasks to task 1 and task 2

--- pomodoro/src/main.rs
+++ pomodoro/src/main.rs
@@ -11,19 +11,18 @@ fn main() -> Result<(), slint::PlatformError> {
 
     // https://docs.rs/slint/latest/slint/struct.ModelRc.html
     let the_model : Rc<VecModel<slint::SharedString>> =
-    Rc::new(VecModel::from(vec!["Hello".into(), "World".into()]));
+    Rc::new(VecModel::from(vec!["Task 1".into(), "Task 2".into()]));
 // Convert it to a ModelRc.
 let the_model_rc = ModelRc::from(the_model.clone());
 // Pass the model to the ui: The generated set_the_model setter from the
 // the_model property takes a ModelRc.
 let cloned_model_for_remove = the_model.clone(); // Clone for use inside the remove-task closure
 
-ui.set_collected_tasks(the_model_rc);
+    ui.set_collected_tasks(the_model_rc);
 
     ui.on_current_task(move |string|{
       // Trim the parsed string
     let parsed = string.trim().to_string();
-    println!("{}", parsed);
     the_model.push(parsed.into());
     
     });
@@ -31,9 +30,9 @@ ui.set_collected_tasks(the_model_rc);
         cloned_model_for_remove.remove(idx.try_into().unwrap());
     });
 
-
+// code to handle the timer
     // code to handle the timer
-    let mut timer = Timer::default();
+    let timer = Timer::default();
     // these varibles will store the time values
     let mut seconds = 0;
     let mut minutes = 0;
@@ -42,15 +41,14 @@ ui.set_collected_tasks(the_model_rc);
         
     
 
-    // ui.on_stop_timer(move ||{
-    //     timer.stop();
-    //     println!("Stopped");
+ // code to handle the timer
+ ui.on_timer_state(move |string| {
+    
 
-    // });
-    ui.on_timer_state(move |string| {
         if string=="start" {
             timer.start(TimerMode::Repeated, std::time::Duration::from_millis(1000), move || {                
                 seconds += 1;
+                
                 if seconds >= 60 {
                     minutes += 1;
                     seconds = 0;
@@ -64,8 +62,10 @@ ui.set_collected_tasks(the_model_rc);
                     minutes = 0;
                     hours = 0;
                 } 
-        
+                // let time_result = format!("Hours: {}, Minutes: {}, Seconds: {}", 0, 0, 0);
+                // ui.set_current_timer_state(time_result.into());
                 println!("Hours: {}, Minutes: {}, Seconds: {}", hours, minutes, seconds);
+                
                 
                 });
         }else if string=="reset" {
@@ -76,7 +76,7 @@ ui.set_collected_tasks(the_model_rc);
             
             println!("reset pressed-- Hours: {}, Minutes: {}, Seconds: {}", hours, minutes, seconds);
 
-            timer.restart();
+            timer.stop();
             timer.restart();
         }else{
             timer.stop();
